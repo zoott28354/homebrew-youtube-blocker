5b562583aa6848fb87fa07fec16a52e969bedf19
giulio <giulio@MacBook-Pro-di-biopixel.local>
2026-04-03 15:58:04 +0200

    Update cask to v1.2.1

--- Casks/youtube-blocker.rb
+++ Casks/youtube-blocker.rb
@@ -1,6 +1,6 @@
 cask "youtube-blocker" do
-  version "1.2.0"
-  sha256 "f3cc96907f5bbcfb34dcc93c7af278c99ebc05d882c0699b29586dce563e13c5"
+  version "1.2.1"
+  sha256 "52f34095c0564a622a9c4a2035fa93c51fdddd2a5f838902b9069686794c8daf"
 
   url "https://github.com/zoott28354/Youtube-Blocker/releases/download/v#{version}/YouTubeBlocker_#{version}_aarch64.dmg"
   name "YouTube Blocker"
@@ -17,19 +17,8 @@ cask "youtube-blocker" do
                    sudo: false
   end
 
-  zap trash: [
-    "/Library/Managed Preferences/com.google.Chrome.plist",
-    "/Library/Managed Preferences/com.microsoft.Edge.plist",
-    "/Library/Managed Preferences/com.brave.Browser.plist",
-    "/Library/Managed Preferences/com.vivaldi.Vivaldi.plist",
-    "/Library/Managed Preferences/com.operasoftware.Opera.plist",
-    "/Library/Managed Preferences/org.chromium.Chromium.plist",
-    "/etc/pf.anchors/com.youtubeblocker",
-  ]
-
   caveats <<~EOS
-    YouTube Blocker requires administrator privileges to modify the hosts file,
-    firewall rules, and browser policies. You will be prompted for your password
-    when launching the app.
+    YouTube Blocker requires administrator privileges to modify the hosts file.
+    You will be prompted for your password when launching the app.
   EOS
 end
